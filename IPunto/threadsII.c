8ea218fffc9ae747e06bfb315c982acebd391b9b
Invitado <invitado@s5pc18.eisc.univalle.edu.co>
2019-08-02 13:11:03 -0500

    añadiendo al rep

---
+++ IPunto/threadsII.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "common.h"
+#include "common_threads.h"
+#include "mycommon.h"
+#include  <time.h>
+
+volatile int counter = 0; 
+int loops;
+puerta door;
+
+void *worker(void *arg) {
+    int i;
+    for (i = 0; i < loops; i++) {
+	cerrar_puerta(door);
+	counter++;
+	abrir_puerta(door);
+    }
+    return NULL;
+}
+
+int main(int argc, char *argv[]) {
+    clock_t timeStart, timeEnd;
+    double secs;
+    timeStart = clock();
+    if (argc != 2) { 
+	fprintf(stderr, "usage: threads <loops>\n"); 
+	exit(1); 
+    } 
+    crear_puerta(door);
+    loops = atoi(argv[1]);
+    pthread_t p1, p2;
+    printf("Initial value : %d\n", counter);
+    Pthread_create(&p1, NULL, worker, NULL); 
+    Pthread_create(&p2, NULL, worker, NULL);
+    Pthread_join(p1, NULL);
+    Pthread_join(p2, NULL);
+    printf("Final value   : %d\n", counter);
+    destruir_puerta(door);
+    timeEnd = clock();
+    secs = (double) (timeEnd - timeStart) / CLOCKS_PER_SEC;
+    printf ("%.16g milisegundos\n", secs * 1000.0);
+
+    return 0;
+}
+
